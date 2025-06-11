cf2b345281a0d416e692dd997098dfa1033d001d
Psc-06 <pradhumanchouhan241@gmail.com>
2025-06-11 20:30:33 +0530

    BackEnd Code

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import { getUserData } from "../controllers/userController.js";
+import userAuth from "../middleware/userAuth.js";
+import express from "express";
+
+const userRouter = express.Router();
+
+userRouter.get("/data", userAuth, getUserData);
+
+export default userRouter;
